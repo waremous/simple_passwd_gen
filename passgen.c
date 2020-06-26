d44b2cb86b29044763d26a6620913dd5b34779f8
AntiduPb <AntiduPb@yandex.ru>
2020-06-26 12:49:24 +0300

    pre Alpha

--- passgen.c
+++ passgen.c
@@ -2,6 +2,9 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
 int getchar_urand() {
 
   int byte_count = 1;
@@ -26,21 +29,63 @@ void main(int argc, char** argv) {
 
   typedef unsigned short BOOL;
 
-  BOOL upper_chars = 0, lower_chars = 0, count_numbers = 0;
-  unsigned int passwd_length = 0;
+  BOOL set_upper_char = FALSE, set_lower_char = FALSE, set_numbers = FALSE;
+  unsigned int passwd_length = 8, passwd_counts = 10;
 
-  if(argc > 1) {
+  for(; passwd_counts > 0; passwd_counts--) {
+    for(int i = passwd_length; i > 0; i--) {
 
-     
+      if(argc == 1) {
+        passwd_length = 8;
+        set_upper_char = TRUE;
+        set_lower_char = TRUE;
+        set_numbers = TRUE;
+      }
 
+      switch (get_rand_range(0, 3)) {
+        case 0:  //If set upper chars.
+          if(set_upper_char) {
+            char c = get_rand_range(65, 91);
+            printf("%c", c);
+            break;
+          }
+          else {
+            passwd_length++;
+            break;
+          }
+        case 1: //If set lower chars.
+          if(set_lower_char) {
+            char c = get_rand_range(97, 123);
+            printf("%c", c);
+            break;
+          }
+          else {
+            passwd_length++;
+            break;
+          }
+        case 2: //If set numbers.
+          if(set_numbers) {
+            char c = get_rand_range(48, 58);
+            printf("%c", c);
+            break;
+          }
+          else {
+            passwd_length++;
+            break;
+          }
+
+      }
+
+    }
+    printf("\n");
   }
 
 
-  char a1 = 0;
-  for (int i = 0; i < 60; i++) {
+/*  char a1 = 0;
+  for (int i = 0; i < 8; i++) {
     a1 = get_rand_range(65, 90);
     printf("%c", a1);
 }
     printf("\n");
-
+*/
 }
